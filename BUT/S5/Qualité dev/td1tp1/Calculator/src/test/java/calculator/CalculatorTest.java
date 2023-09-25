33e03dfc447d2fa4e6474cfbbee3125b7b545c6a
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-09-25 16:56:05 +0200

    Codage des méthodes de test sur la méthode add, codage de plusieurs méthodes pour divide

--- BUT/S5/Qualité dev/td1tp1/Calculator/src/test/java/calculator/CalculatorTest.java
+++ BUT/S5/Qualité dev/td1tp1/Calculator/src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,189 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
 
 class CalculatorTest {
+    @Test
+    void add_additionne_int_positifs(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 2;
+        int opD = 3;
 
+        //When
+        result = calcul.add(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(5);
+    }
+
+  @Test
+    void add_additionne_int_positif_negatif(){
+      // Given
+      Calculator calcul = new Calculator();
+      int result;
+      int opG = 2;
+      int opD = -3;
+
+      //When
+      result = calcul.add(opG, opD);
+
+      //Then
+      assertThat(result).isEqualTo(-1);
+  }
+
+  @Test
+    void add_additionne_int_negatifs(){
+      // Given
+      Calculator calcul = new Calculator();
+      int result;
+      int opG = -2;
+      int opD = -3;
+
+      //When
+      result = calcul.add(opG, opD);
+
+      //Then
+      assertThat(result).isEqualTo(-5);
+    }
+
+    @Test
+    void add_additionne_int_positif_null(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 2;
+        int opD = 0;
+
+        //When
+        result = calcul.add(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void add_additionne_int_nulls(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 0;
+        int opD = 0;
+
+        //When
+        result = calcul.add(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void add_additionne_large_int(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 50000000;
+        int opD = 50000000;
+
+        //When
+        result = calcul.add(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(100000000);
+    }
+
+    @Test
+    void divide_divise_int_positifs(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 4;
+        int opD = 2;
+
+        //When
+        result = calcul.divide(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void divide_divise_int_positif_negatif(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 4;
+        int opD = -2;
+
+        //When
+        result = calcul.divide(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(-2);
+    }
+
+    @Test
+    void divide_divise_int_negatifs(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = -4;
+        int opD = -2;
+
+        //When
+        result = calcul.divide(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void divide_divise_int_positif_null(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 4;
+        int opD = 0;
+
+        //When
+        result = calcul.divide(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(null);
+    }
+
+    @Test
+    void divide_divise_int_nulls(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 0;
+        int opD = 0;
+
+        //When
+        result = calcul.divide(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void divide_divise_large_int(){
+        // Given
+        Calculator calcul = new Calculator();
+        int result;
+        int opG = 40000000;
+        int opD = 20000000;
+
+        //When
+        result = calcul.divide(opG, opD);
+
+        //Then
+        assertThat(result).isEqualTo(2);
+    }
 }
